51fde9552504d3afcdd6f63bf73229e5ab0a60a0
Patrick Varin <patrickjvarin@gmail.com>
2017-06-09 21:59:54 -0400

    wrote a better valuecheck.m utility

--- drake/matlab/util/valuecheck.m
+++ drake/matlab/util/valuecheck.m
@@ -1,79 +1,65 @@
 function [tf,errstr] = valuecheck(val,desired_val,tol)
 
 % VALUECHECK
-%   Usage:  valuecheck(val,desired_val)
+%   Usage:  valuecheck(val,desired_val,tol)
+%
+%   If no output is specified valuecheck throws an error if the values are
+%   unequal, otherwise it returns a boolean and a message describing what,
+%   if anything is unequal
 
-if (nargin<3 || isempty(tol)) tol=1e-8; end
+if (nargin<3 || isempty(tol))
+    tol=1e-8;
+end
 
 tf = true;
 errstr = '';
 
+% broadcast scalar expected value to correct size
 if isscalar(desired_val) && ~isempty(val)
   desired_val = repmat(desired_val,size(val));
 end
 
+% check sizes
 if ((length(size(val))~=length(size(desired_val))) || any(size(val)~=size(desired_val)))
-  errstr = ['Wrong size.  Expected ', mat2str(size(desired_val)),' but got ', mat2str(size(val))];
+  errstr = sprintf('Wrong size.  Expected %s but got %s.', mat2str(size(desired_val)), mat2str(size(val)));
   tf = false;
   if (nargout>0)
     return;
   end
 end
 
-if (tf && ~isequal(isnan(val(:)),isnan(desired_val(:))))
-  errstr = 'NANs don''t match. ';
-  if any(isnan(val(:)))
-    val
-    [a,b] = ind2sub(find(isnan(val(:))),size(val));
-    errstr = [errstr,sprintf('Found NANs at \n'),sprintf('(%d,%d) ',[a;b]),sprintf('\n')];
-  else
-    errstr = [errstr,'val has no NANs'];
-  end
-  if any(isnan(desired_val(:)))
-    desired_val
-    [a,b] = ind2sub(size(desired_val),find(isnan(desired_val(:))));
-    errstr = [errstr,sprintf('but desired_val has them at \n'), sprintf('(%d,%d)',[a;b]),sprintf('\n')];
-  else
-    errstr = [errstr,'but desired_val has no NANs'];
-  end
-  %    err = desired_val-val;
-  %    err(abs(err)<tol)=0;
-  %    err=sparse(err)
-  
-  tf = false;
-  if (nargout>0)
-    return;
-  end  
-end
+% check NAN positions
+nan_idx_val = isnan(val(:));
+nan_idx_des = isnan(desired_val(:));
+tf = tf & isequal(nan_idx_val,nan_idx_des);
 
-if (tf && any(abs(val(:)-desired_val(:))>tol))
-  if (ndims(val)<=2 && length(val)<=6)
+% check numeric values
+val_idx = ~nan_idx_val & ~nan_idx_des;
+if any(abs(val(val_idx)-desired_val(val_idx)) > tol)
+  if (ndims(val)<=2 && length(val)<=6) % for small 2-d arrays print the whole matrix
     % clean before printing
     desired_val(abs(desired_val)<tol/2)=0;
     val(abs(val)<tol/2)=0;
     errstr = sprintf('Values don''t match.  Expected \n%s\n but got \n%s', mat2str(desired_val), mat2str(val));
-  else
+  else % for large matrices or N-d arrays print only the mismatched values 
     err = desired_val-val;
-    errstr = size(desired_val);
-    % print sparse-matrix-like format, but support ND arrays:
-    ind=find(abs(err(:))>tol);
-    a = cell(1,length(errstr));
-    [a{:}] = ind2sub(errstr,ind);
-    errstr = '';
+    ind=find(abs(err(:))>tol | (nan_idx_val(:) ~= nan_idx_des(:)));
+    val_size = size(desired_val);
+    a = cell(1,length(val_size));
+    [a{:}] = ind2sub(val_size,ind);
+    errstr_cell_array = cell(numel(ind),1);
     for i=1:numel(ind)
-      b = cellfun(@(b) b(i),a);
-      indstr = ['(',sprintf('%d,',b(1:end-1)), sprintf('%d)',b(end))];
-      errstr = [errstr, sprintf('%10s %12f %15f\n',indstr,full(val(ind(i))),full(desired_val(ind(i))))];
+      b = cellfun(@(b) {num2str(b(i))},a);
+      indstr = ['(',strjoin(b,','),')'];
+      errstr_cell_array{i} = sprintf('%10s %12f %15f\n',indstr,full(val(ind(i))),full(desired_val(ind(i))));
     end
-    errstr = sprintf('Values don''t match.\n    Index       Value       Desired Value\n   -------     --------    ---------------\n%s', errstr);
+    errstr = sprintf('Values don''t match.\n    Index       Value       Desired Value\n   -------     --------    ---------------\n%s', [errstr_cell_array{:}]);
   end
   
   tf = false;
-  if (nargout>0)
-    return;
-  end
 end
 
-if ~tf, 
+% if the values are not equal and there are no outputs throw an error
+if ~tf && (nargout == 0)
   error('Drake:ValueCheck',errstr); 
 end
