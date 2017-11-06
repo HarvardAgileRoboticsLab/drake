78c850d1f4b7b77d8f1fd60cef4e79d286a33aa4
Neel Doshi <nddoshi9@gmail.com>
2017-11-06 13:56:05 -0500

    small fixes to fitting pipeline

--- drake/examples/HAMR-URDF/dev/SimpleHAMR/SimpleSLFitSpringDamper.m
+++ drake/examples/HAMR-URDF/dev/SimpleHAMR/SimpleSLFitSpringDamper.m
@@ -33,23 +33,31 @@ xin = xi(SL.getActuatedJoints(), :);
 Trat = xin'\xout';
 Trat_sq = Trat*Trat;
 
-K0 = diag(3*0.05*ones(2,1)) + Trat_sq\diag([1.1556; 1.1556]);
-P0 = diag(0.01*ones(2,1)) + Trat_sq\diag([0.01; 0.01]);
+K0 = zeros(numel(sfb_inputs)); %-diag(3*0.05*ones(2,1)) - Trat_sq\diag([1.1556; 1.1556]);
+P0 = zeros(numel(sfb_inputs)); %-diag(0.01*ones(2,1)) - Trat_sq\diag([0.01; 0.01]);
 
 options.k = K0(:);
 options.p = P0(:);
 
 SLSimple = SLSimpleRBM(sl_urdf, options);
 viz = SLSimple.constructVisualizer();
+
 x0 = SLSimple.getInitialState();
-viz.inspector(x0);
 nqS = SLSimple.getNumPositions();
 nvS = SLSimple.getNumVelocities();
 
-%% Fitting
+tau_traj = PPTrajectory(foh(t, tau));
+tau_traj = tau_traj.setOutputFrame(SLSimple.getInputFrame());
 
+% Simulate with initial guess
 i_start = find(t >= 1000, 1, 'first');
-i_end = numel(t);
+i_end = i_start+1000;
+
+SLSimple_OL0 = cascade(tau_traj, SLSimple);
+xtraj0 = simulate(SLSimple_OL0, [t(1), t(i_end)], xi([sfb_inputs; nq+sfb_inputs], 1));
+xx0 = xtraj0.eval(t);
+
+%% Fitting
 
 params.SL = SL;
 params.SLSimple = SLSimple;
@@ -66,23 +74,14 @@ ub = Inf(size(z0));
 optopt = optimoptions('lsqnonlin', 'SpecifyObjectiveGradient', true);
 [zopt,resnorm,residual,exitflag,output] = lsqnonlin(@(z0)SLFitFun(z0, params),z0,lb,ub,optopt);
 
-SLSimple = SLSimple.setK(reshape(zopt(1:nqS^2), nqS, nqS));
-disp(SLSimple.K);
-SLSimple = SLSimple.setP(reshape(zopt(nqS^2+(1:nvS^2)), nvS, nvS));
-disp(SLSimple.P);
+SLSimple = SLSimple.setK(reshape(zopt(1:nqS^2), nqS, nqS)); disp(SLSimple.K);
+SLSimple = SLSimple.setP(reshape(zopt(nqS^2+(1:nvS^2)), nvS, nvS)); disp(SLSimple.P);
 SLSimple = SLSimple.removeAllStateConstraints();
 
 %% Verify
-;
-% SLSimpleTSRBM = TimeSteppingRigidBodyManipulator(SLSimple, options.dt, options);
-
-tau_traj = PPTrajectory(foh(t, tau));
-tau_traj = tau_traj.setOutputFrame(SLSimple.getInputFrame());
 SLSimple_OL = cascade(tau_traj, SLSimple);
-xtraj_fit = simulate(SLSimple_OL, [t(1), t(end)], xi([sfb_inputs; nq+sfb_inputs], 1));
-
-ttfit = xtraj_fit.getBreaks();
-xxfit = xtraj_fit.eval(ttfit);
+xtraj_fit = simulate(SLSimple_OL, [t(1), t(i_end)], xi([sfb_inputs; nq+sfb_inputs], 1));
+xxfit = xtraj_fit.eval(t);
 %% Plotting
 ind_plot = [sfb_inputs; nq+sfb_inputs];
 figure(1); clf; hold on;
@@ -91,9 +90,12 @@ for i = 1:numel(ind_plot)
     subplot(2,2,i); hold on;
     title(titles{i});
     plot(t, xi(ind_plot(i), :))
-    plot(ttfit, xxfit(i, :) - x0(i))
+    plot(t, xx0(i, :) - x0(i));
+    plot(t, xxfit(i, :) - x0(i))
 end
 
-xtraj_fit_scaled = PPTrajectory(foh(ttfit*1e-3, xxfit));
+xtraj_fit_scaled = PPTrajectory(foh(t*1e-3, xxfit));
 xtraj_fit_scaled = xtraj_fit_scaled.setOutputFrame(xtraj_fit.getOutputFrame());
 viz.playback(xtraj_fit_scaled, struct('slider', true));
+
+rms(xi(ind_plot, :) - (xxfit - x0),2)
